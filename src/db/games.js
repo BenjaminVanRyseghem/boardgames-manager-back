cca35de960887b2120b338b6566552a4adaf1928
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2020-10-11 00:06:50 +0200

    Fix fuzzy matching for accents

--- src/db/games.js
+++ src/db/games.js
@@ -26,7 +26,11 @@ const actions = {
 };
 
 function nameOfGame(nameFragment) {
-	return (datum) => datum.name.match(new RegExp(nameFragment, "i"));
+	let normalizedFragment = nameFragment.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	return (datum) => {
+		let normalizedDatum = datum.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+		return normalizedDatum.match(new RegExp(normalizedFragment, "i"));
+	};
 }
 
 function numberOfPlayers(number) {
@@ -265,7 +269,7 @@ function findInLocation(location) {
 function countByUser({ id }) {
 	return db
 		.then((data) => data.filter((game) => game.borrowed &&
-				(game.borrowed === id || game.borrowed.id === id)))
+			(game.borrowed === id || game.borrowed.id === id)))
 		.then((data) => data.value())
 		.then((games) => games.length);
 }
@@ -273,7 +277,7 @@ function countByUser({ id }) {
 function findByUser(user) {
 	return db
 		.then((data) => data.filter((game) => game.borrowed &&
-				(game.borrowed === user || game.borrowed.id === user)))
+			(game.borrowed === user || game.borrowed.id === user)))
 		.then((data) => data.value())
 		.then((games) => Promise.all(games.map((game) => normalizeGame(game))));
 }
@@ -301,42 +305,7 @@ db
 	.catch(() => {
 		low(adapter)
 			.then((database) => database.defaults({
-				games: [
-					{
-						id: "1",
-						name: "Zombicide",
-						description: "Zombicide is a collaborative game in which players take the role of a survivor &amp;ndash; each with unique abilities &amp;ndash; and harness both their skills and the power of teamwork against the hordes of unthinking undead! Zombies are predictable, stupid but deadly, controlled by simple rules and a deck of cards. Unfortunately for you, there are a LOT more zombies than you have bullets.&amp;#10;&amp;#10;Find weapons, kill zombies. The more zombies you kill, the more skilled you get; the more skilled you get, the more zombies appear. The only way out is zombicide!&amp;#10;&amp;#10;Play ten scenarios on different maps made from the included modular map tiles, download new scenarios from the designer's website, or create your own!&amp;#10;&amp;#10;&amp;#10;     This is just a great game for zombie lovers!&amp;#10;&amp;#10;&amp;#10;Integrates with:&amp;#10;&amp;#10;    Zombicide Season 2: Prison Outbreak&amp;#10;    Zombicide Season 3: Rue Morgue&amp;#10;&amp;#10;&amp;#10;&amp;#10;&amp;#10;",
-						minPlayers: 1,
-						maxPlayers: 5,
-						minPlaytime: 90,
-						maxPlaytime: 180,
-						minAge: 13,
-						borrowed: "12",
-						picture: "https://cf.geekdo-images.com/original/img/FwnbGGrU7av4j8kB11VZZRB58U4=/0x0/pic1196191.jpg",
-						location: "7",
-						publishers: ["1"],
-						yearPublished: "2012",
-						categories: ["1"],
-						mechanics: ["1"]
-					},
-					{
-						id: "2",
-						name: "Forbidden Desert",
-						description: "Game description from the publisher:&amp;#10;&amp;#10;Gear up for a thrilling adventure to recover a legendary flying machine buried deep in the ruins of an ancient desert city. You'll need to coordinate with your teammates and use every available resource if you hope to survive the scorching heat and relentless sandstorm. Find the flying machine and escape before you all become permanent artifacts of the forbidden desert!&amp;#10;&amp;#10;In Forbidden Desert, a thematic sequel to Forbidden Island, players take on the roles of brave adventurers who must throw caution to the wind and survive both blistering heat and blustering sand in order to recover a legendary flying machine buried under an ancient desert city. While featuring cooperative gameplay similar to Forbidden Island, Forbidden Desert is a fresh, new game based around an innovative set of mechanisms such as an ever-shifting board, individual resource management, and a unique method for locating the flying machine parts.&amp;#10;&amp;#10;",
-						minPlayers: 2,
-						maxPlayers: 5,
-						minPlaytime: 45,
-						maxPlaytime: 45,
-						borrowed: null,
-						minAge: 10,
-						picture: "https://cf.geekdo-images.com/original/img/wYvf6LExNhb3rflp_QYmCK_NhMc=/0x0/pic1528722.jpg",
-						location: "7",
-						publishers: ["1"],
-						yearPublished: "2013",
-						categories: ["1"],
-						mechanics: ["1"]
-					}
-				]
+				games: []
 			}))
 			.then((database) => database.write());
 	});
