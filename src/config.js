3615f6867c4e6443b574de1398747c096909f530
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2021-02-28 01:14:16 +0100

    Add https support

--- src/config.js
+++ src/config.js
@@ -2,7 +2,8 @@
 const config = {
 	environment: process.env.NODE_ENV || "dev",
 	server: {
-		port: process.env.PORT || 8080
+		port: process.env.PORT || 80,
+		sslPort: process.env.SSL_PORT || 443
 	},
 	jwt: {
 		secret: process.env.JWT_SECRET,
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
 
+let { redirectToHTTPS } = require("express-http-to-https");
 const bodyParser = require("body-parser");
 const express = require("express");
 const helmet = require("helmet");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 const morgan = require("morgan");
-const path = require("path");
 const routes = require("./routes");
 
 const config = require("./config");
@@ -16,6 +20,7 @@ const entityDecoder = require("./middlewares/entityDecoder");
 // initialise express
 let app = express();
 
+app.use(redirectToHTTPS());
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -39,7 +44,17 @@ app.get("/", (req, res) => {
 	res.render("index.html");
 });
 
+if (process.env.USE_SSL && process.env.KEY && process.env.CERT) { // eslint-disable-line no-process-env
+	let options = {
+		key: fs.readFileSync(process.env.KEY), // eslint-disable-line no-sync,no-process-env
+		cert: fs.readFileSync(process.env.CERT) // eslint-disable-line no-sync,no-process-env
+	};
+	let index = https.createServer(options, app).listen(config.server.sslPort, () => {
+		console.log("\nSecured server ready on port %d\n", index.address().port); // eslint-disable-line no-console
+	});
+}
+
 // start the server
-let index = app.listen(config.server.port, () => { // eslint-disable-line no-process-env
+let index = http.createServer(app).listen(config.server.port, () => { // eslint-disable-line no-process-env
 	console.log("\nServer ready on port %d\n", index.address().port); // eslint-disable-line no-console
 });
